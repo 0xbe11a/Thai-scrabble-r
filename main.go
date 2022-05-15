1c4fda2bedf9903a3d6aecbfb1b4bc55f11eb226
nubellachan <nu.sakurachan@gmail.com>
2022-05-15 18:35:34 +0700

    read static thai dict json file

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
+
 	fmt.Println("Hello, World!")
+	fileContent, err := os.Open("../thai-wordlist/thai-wordlist.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Read file success!")
+	defer fileContent.Close()
 }
